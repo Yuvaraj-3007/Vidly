d25b0b5420a09d28c11ad1c1d036d6e9a10c42d0
Yuvaraj Pandian <yuva3007raj@gmail.com>
2026-01-30 09:00:11 +0530

    Add health check endpoint to backend root

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,18 @@
 const express = require("express");
-const cors = require("cors");
-const homeRoutes = require("./routes/index");
-const movieRoutes = require("./routes/movies");
+const movies = require("./routes/movie");
 
 const app = express();
-app.use(cors());
+
 app.use(express.json());
-app.use("/api/", homeRoutes);
-app.use("/api/movies", movieRoutes);
+
+app.get("/", (req, res) => {
+  res.json({ status: "ok", message: "Vidly API is running" });
+});
+
+app.use("/api/movies", movies);
 
 module.exports = app;
+
+
+
+
